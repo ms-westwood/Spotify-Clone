593778975182ba06c23c82bdcc5199b992808a80
riri <richellelynncipriano@gmail.com>
2026-03-06 10:20:47 -0800

    Initial Flutter project setup

---
+++ client/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:client/core/team/theme.dart';
+import 'package:client/features/auth/view/pages/signup_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: AppTheme.darkThemeMode,
+      home: const SignupPage(),
+    );
+  }
+}
+
