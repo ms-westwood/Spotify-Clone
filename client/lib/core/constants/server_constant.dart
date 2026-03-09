44a4035e289cfccbc41796ebb95a3810d7142ca1
riri <richellelynncipriano@gmail.com>
2026-03-09 15:21:06 -0700

    added cross platform compatibility: ios, android, web

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -1,3 +1,17 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
+
 class ServerConstant {
-  static const String serverURL = "http://127.0.0.1:8000";
+  static String get serverURL {
+    if (kIsWeb) {
+      // Web cannot use Platform; assume backend runs on localhost
+      return "http://127.0.0.1:8000";
+    } else if (Platform.isAndroid) {
+      // Android emulator
+      return "http://10.0.2.2:8000";
+    } else {
+      // iOS simulator / desktop
+      return "http://127.0.0.1:8000";
+    }
+  }
 }
